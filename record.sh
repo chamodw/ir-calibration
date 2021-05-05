da225beddd9ccc14f47ac14c2b3804719d458960
Chamod <chamod@ahlab.org>
2021-05-05 16:23:39 +1200

    Change temperature input to cmd args to save time

--- record.sh
+++ record.sh
@@ -19,16 +19,14 @@ ELF2DFU=elf2dfu/elf2dfu
 DFU_UTIL=dfu-util
 CALIB_SCRIPT=calibration/calibration.py
 
-SENSOR_DATA=data_tue
+SENSOR_DATA=wed_data
 
 PYTHON=python3.9
 
 echo "--------------------------------"
-echo Please enter temperature reading 1
 
-read TEMP1
-
-
+TEMP1=$1
+echo $TEMP1
 
 #Extract serial number from the lsusb output
 ID=$(lsusb 2> /dev/null | grep "Kiwrious Temperature" | awk '{print $NF}' )
@@ -60,15 +58,15 @@ mkdir $SENSOR_DATA/$ID
 
 timeout $TIMEOUT  unbuffer hexdump -v -e " \"$TEMP1, \" 13/2 \"%6u, \"  \"\n\" " $PORT  >> $SENSOR_DATA/$ID/data.txt
 
-exit 0
+
 
 #---------------------------------------------------------------
 # Invoking Python script for calibration
 #---------------------------------------------------------------
 
-$PYTHON $CALIB_SCRIPT $(pwd)/$SENSOR_DATA/$ID/data.txt $(pwd)/$SRC_PATH/ir_constants.h
+$PYTHON dev.py $(pwd)/$SENSOR_DATA/$ID/data.txt
 
-
+exit 0
 
 #--------------------------------------------------------------
 # Rebuilding firmware
