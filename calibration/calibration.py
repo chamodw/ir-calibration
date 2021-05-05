b3957989aea8732c9f516996bf5935c6e02e54f3
Chamod <chamod@ahlab.org>
2021-05-05 16:34:35 +1200

    Adjust stdev error range:

--- calibration/calibration.py
+++ calibration/calibration.py
@@ -87,7 +87,7 @@ for i in range(len(all_data)):
 		print ("\t ", avg, " \t\t ", all_data[i][1])
 		print ("\t ", round(stdev, 2), " \t\t ", round(new_stdev,2));
 
-		if (new_stdev >= 30):
+		if (new_stdev >= 40):
 			print ("***Error too high! ***");
 			print ("***Press enter to continue ***");
 			inp = input();
