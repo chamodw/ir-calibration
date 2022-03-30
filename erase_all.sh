5d075e449a4e38a793ea5f2f5bd5a886f5b36e35
ahl-local <ahl-local@wifi-staff-172-24-50-177.net.auckland.ac.nz>
2022-03-30 22:11:49 +1300

    Add script to erase all data for connected sensor

---
+++ erase_all.sh
@@ -0,0 +1,57 @@
+#!/bin/zsh
+
+#	
+#	Chamod Weerasinghe
+#	Data inspection script
+#	Calculates standard deviation and highlights errors
+#	
+
+
+TIMEOUT=5 # Specifies the data collection time for each temperature point
+
+SRC_PATH=sensor-build/kiwrious-sensor/src
+MAKE_PATH=sensor-build
+ELF2DFU=elf2dfu/elf2dfu
+DFU_UTIL=dfu-util
+CALIB_SCRIPT=calibration/calibration.py
+PLOT=0 #  plot level
+
+SENSOR_DATA=session2_thu
+
+PYTHON=python3
+
+
+
+#Extract serial number from the lsusb output
+ID=$(util/serial.sh)
+
+
+# If serial number is empty, no sensor is connected
+if [ -z "$ID" ] ; then
+	echo "No sensor found"
+	exit
+fi
+
+
+echo "--------------------------------"
+echo "Sensor found with serial number $ID"
+
+
+# Look for all possible folders for this sensor
+A=$(ls session2_thu | grep $ID)
+#B=$(ls fri_data | grep $ID)
+
+if [[ ! -z $A ]] ; then
+	SENSOR_DATA=session2_thu
+	echo "Session2 Thursday"
+#else
+#	SENSOR_DATA=fri_Data
+#	echo "Friday"
+fi
+
+
+
+# Look for data in  different folders
+ls session2_thu/$ID
+
+rm session2_thu/$ID/*
